08d3b1cbd031410bcbfc4307b8b7e2937a1b6b4c
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-07-04 00:40:18 +0200

    Animations and services optimization done

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpParams} from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Country } from '../interfaces/country.interface';
+import { tap } from 'rxjs/operators';
 
 
 
@@ -12,16 +13,21 @@ export class CountriesService {
 
   private apiURL: string="https://restcountries.eu/rest/v2";
 
+  get obtainHttpParams(){
+    return new HttpParams()
+              .set('fields', 'name;capital;alpha2Code;flag;population');
+  }
+
   constructor(private http:HttpClient) { }
 
   searchCountry(term:string):Observable<Country[]>{
     const url=`${this.apiURL}/name/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.obtainHttpParams});
   }
 
   searchCountryByCapital(term:string):Observable<Country[]>{
     const url=`${this.apiURL}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.obtainHttpParams});
   }
 
   getCountryByCode(code:string):Observable<Country>{
@@ -30,7 +36,11 @@ export class CountriesService {
   }
 
   getCountriesByRegion(region:string):Observable<Country[]>{
+
     const url=`${this.apiURL}/region/${region}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.obtainHttpParams})
+    .pipe(
+      tap(console.log)
+    )
   }
 }
